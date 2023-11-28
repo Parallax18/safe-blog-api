aa49ffdd3b5d78732b5236e5356b058fc5f324b4
Parallax18 <rhythmist204@gmail.com>
2023-11-28 14:44:26 +0100

    chore: add user status

--- app/models/user.js
+++ app/models/user.js
@@ -14,6 +14,10 @@ const schema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    status: {
+      type: String,
+      default: "active",
+    },
   },
   { timestamps: true }
 );
