539ce06279a292cfe0bd2c1de8035b4ac1d2cbc3
Parallax18 <rhythmist204@gmail.com>
2023-11-28 13:17:55 +0100

    fix: add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 
 const dbConfig = require("./config/database.config.js");
